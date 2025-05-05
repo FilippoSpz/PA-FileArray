db5074421b3e9e83cb15b3b5e68f58aea23d4e48
Filippo <spazzalifilippo@icloud.com>
2025-05-05 17:13:18 +0200

    file array zip

--- MainGraph.java
+++ MainGraph.java
@@ -3,7 +3,6 @@ import java.io.File;
 public class MainGraph {
     public static void main(String[] args) {
         for (int n = 0; n <= 100; n++) {
-            ;
 
             long gzipFileLength = new File("testgraph.zip").length();
             long binFileLength = new File("testgraph.bin").length();
